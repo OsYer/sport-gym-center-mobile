85a2921830f7e869298919b0d30a151a0f7934af
OsYer <20210720@uthh.edu.mx>
2024-09-24 17:24:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sport Gym Center Mobile',
+  webDir: 'dist'
+};
+
+export default config;
